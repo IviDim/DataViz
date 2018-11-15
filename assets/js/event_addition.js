bd528f17ad8845cd7ecee060f19acb190a33d189
Ivi Dimopoulou <ivi.dimopoulou@epfl.ch>
2018-11-15 03:34:32 +0100

    Add events checkboxes to visualization

--- assets/js/event_addition.js
+++ assets/js/event_addition.js
@@ -1,13 +1,47 @@
 
-// TODO Change to actual data location
-events = d3.csv("http://localhost:8000/events_2013_2018_sorted.csv", function(d) {
-	
-	return {
-		event_id: d.id, 
-		event_name: d.name,
-		event_date: d.date,
-		event_category: d.category
-	};
-});
+d3.dsv(",", "https://ividim.github.io/DataViz/data/events_2013_2018_sorted.csv",
+	function(d) {
 
-console.log(events);
+		return {
+			event_id: d.id, 
+			event_name: d.name,
+			event_date: d.date,
+			event_month: d.month2,
+			event_year: d.year,
+			event_category: d.category
+		};
+	})
+.then (
+	function(events) {		
+
+		const categories = 
+					["Politics", "Social", "Science", "Sports", "Viral"];
+
+		// Append a checkbox for each event, in the appropriate category
+		categories.map(categ => events.filter(
+							x => x.event_category === categ))
+					.forEach(function(arr, index) {
+
+			const li = d3.select("#events-" + categories[index].toLowerCase())
+						.select(".card-body")
+						.select("ul")
+						.selectAll(".event-checkbox")
+						.data(arr)
+						.enter()
+						.append("li")
+							.classed("list-group-item", true)
+							.classed("event-checkbox", true)
+							.classed("inline-field", true);
+				
+			li.append("input")
+				.attr("type", "checkbox")
+				.attr("id", d => "event" + d.event_id);
+
+
+			li.append("label")
+				.attr("for", d => "event" + d.event_id)
+				.text(d => d.event_name + 
+					" [" + d.event_month + " " + d.event_year +"]");
+		});
+	});
+
