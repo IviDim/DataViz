dacf50ede7a530d04779e9e7d3edbec90bd369fe
Ivi Dimopoulou <ivi.dimopoulou@epfl.ch>
2018-12-21 20:57:33 +0100

    Newest version (after re-color).

--- assets/js/init_viz.js
+++ assets/js/init_viz.js
@@ -1,9 +1,7 @@
 // Initialize global variables
 
-// Initial date  //TODO Change appropriately
-// WATCH OUT! Month is an index, beginning from 0 !!
-// const initial_dates = [new Date(2014, 0, 1), new Date(2018, 11, 31)];
-const initial_dates = [new Date(2017, 0, 1), new Date(2018, 10, 30)];
+// Initial dates
+const initial_dates = [new Date(2016, 0, 1), new Date(2018, 10, 30)];
 
 // Viz elements
 let scatterplot;   	//Stores instance of scatterplot class
@@ -16,12 +14,12 @@ let events;			//Stores instance of events class
 const circle_radius = 2.5;   	//Radius of circle on top articles view
 
 let selected_events_list = []; 	//List of events that have been selected on 
-								// the left side panel
+                                  // the left side panel
 
 // State
 let state = "TopArticles";	// Global state of the viz (either "TopArticles" 
-							// or "SingleArticle").
+// or "SingleArticle").
 
 let ignore_event = false;	// Prevents brush event propagation, when
-							// the brush selection is rounded, for 
-							// better visualization. 
+// the brush selection is rounded, for
+// better visualization.
